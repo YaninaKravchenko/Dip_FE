bc2391d6d231efce77eb7c78a273bae67953dc82
YaninaKravchenko <yakrauchanka@gmail.com>
2023-10-21 12:45:28 +0300

    Add favorite feature

--- book-store/src/components/Posts/PostBookDetail.tsx
+++ book-store/src/components/Posts/PostBookDetail.tsx
@@ -4,13 +4,28 @@ import { BookResponse } from '../../types';
 import { useParams } from 'react-router-dom';
 import StarRating from '../StarRating/StarRating';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../Store';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { myFavoritesActions } from '../../Store/Actions/myFavoritesActions';
 
 const PostBookDetail: React.FC = () => {
   const { isbn13 } = useParams<{ isbn13: string }>();
   const [book, setBook] = useState<BookResponse | null>(null);
+  const dispatch = useDispatch();
+  const favoriteBooks = useSelector(
+    (state: RootState) => state.favorites.favorite
+  );
+
+  const bookIsFavorite = favoriteBooks.some((book) => book.isbn13 === isbn13);
 
   useEffect(() => {
     const fetchBookDetails = async () => {
+      if (!isbn13) {
+        console.error('ISBN is undefined.');
+        return;
+      }
+
       try {
         const response = await fetch(
           `https://api.itbook.store/1.0/books/${isbn13}`
@@ -27,10 +42,28 @@ const PostBookDetail: React.FC = () => {
 
   if (!book) return <div>Loading...</div>;
 
+  const handleToggleFavorite = () => {
+    if (!isbn13) {
+      console.error('ISBN is undefined.');
+      return;
+    }
+
+    if (bookIsFavorite) {
+      dispatch(myFavoritesActions.removeFromFavorite(isbn13));
+    } else {
+      dispatch(myFavoritesActions.addToFavorite(book));
+    }
+  };
+
   return (
     <div>
       <h1>{book.title}</h1>
-      <FavoriteBorderIcon />
+      {bookIsFavorite ? (
+        <FavoriteIcon onClick={handleToggleFavorite} style={{ color: 'red' }} />
+      ) : (
+        <FavoriteBorderIcon onClick={handleToggleFavorite} />
+      )}
+
       <img src={book.image} alt={book.title} />
       <StarRating rating={book.rating} />
       <p>
